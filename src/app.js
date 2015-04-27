bf1b732db9c11e51de45d66be3cd4ca3d503dcc0
evanhackett <evanwhackett@gmail.com>
2015-04-27 16:01:22 -0700

    pixel scaling and pixel placing seem to work

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
+
   var chip = new Chip8();
+
+  // test to see if pixel scaling and pixel placing work correctly on render
   var screenBuffer = new Uint8Array(64*32);
+  screenBuffer[128] = 1;
   chip.display.render(screenBuffer);
+
 });
--- src/display.js
+++ src/display.js
@@ -2,13 +2,33 @@
 var Display = function() {
   var display = {};
 
+  // this function takes the screenBuffer and draws white pixels where 1's occur and Black pixels where 0's occur
   display.render = function(screenBuffer) {
+
     var canvas = $('#chip8Screen')[0];
     var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var height = canvas.height;
+    // since 64 x 32 is really small, we made a 640 x 320 canvas. This means we have to scale up our pixels
+    var scaleFactor = Math.floor(width / 64);
 
     // we want 'on' pixels to be white
+    // and 'off' pixels to be black
+    
+    // first we reset the screen to all black
     ctx.fillStyle = "#000";
-    ctx.fillRect(10,10,55,50);
+    ctx.fillRect(0, 0, width, height);
+
+    // loop through the screenBuffer, if the bit is white, draw a white rect
+    ctx.fillStyle = "#fff";
+    var x, y;
+    for (var i = 0; i < screenBuffer.length; i++) {
+      x = (i % 64) * scaleFactor;
+      y = Math.floor(i / 64) * scaleFactor;
+      if (screenBuffer[i]) {
+        ctx.fillRect(x, y, scaleFactor, scaleFactor);
+      }
+    }
   };
 
 
