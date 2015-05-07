496562d3ff6d9b3e87ad7c3fb7d5fd89abb2d417
evanhackett <evanwhackett@gmail.com>
2015-05-07 15:09:28 -0700

    setup testing using generator-test

---
+++ spec/chip8.js
@@ -0,0 +1,21 @@
+/* global chip8, describe, it, expect, should */
+
+describe('Chip8()', function () {
+  'use strict';
+
+
+  it('exists', function () {
+    expect(Chip8).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
